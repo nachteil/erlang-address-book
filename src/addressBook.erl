c62d883587d09d9e56dc66a4a40470521e004dc3
yorg <nachteil@gmail.cp>
2015-01-10 17:34:05 +0100

    after some refactoring

--- src/addressBook.erl
+++ src/addressBook.erl
@@ -17,26 +17,134 @@
 }).
 
 %% API
--export([createAddressBook/0, addContact/3]).
+-export([createAddressBook/0, addContact/3, addEmail/4, addPhone/4, removeContact/3, removeEmail/2, getEmails/3, getPhones/3, findByPhone/2, findByEmail/2]).
 
+getBy(Predicate, AddressBook, FilterValue) ->
+  Filter = case Predicate of
+             emil -> fun(CurrentEntry) -> lists:member(FilterValue, CurrentEntry#entry.emails) end ;
+             phone -> fun(CurrentEntry) -> lists:member(FilterValue, CurrentEntry#entry.phones) end ;
+             fullName -> fun(CurrentEntry) -> CurrentEntry#entry.fullName == FilterValue end ;
+             _ -> fun(CurrentEntry) -> 1 == 0 end
+           end,
+  lists:filter(Filter, AddressBook)
+.
+
+modifyEntry(FieldType, Operation, Entry, ModificationValue) ->
+  {Phones, Emails} =  case FieldType of
+                        phone -> case Operation of
+                                   remove -> {Entry#entry.phones -- [ModificationValue], Entry#entry.emails} ;
+                                   add -> {[ModificationValue | Entry#entry.phones], Entry#entry.emails}
+                                 end ;
+                        emil -> case Operation of
+                                  remove -> {Entry#entry.phones, Entry#entry.emails -- [ModificationValue]} ;
+                                  add -> {Entry#entry.phones, addEmil, [ModificationValue | Entry#entry.emails]}
+                                end
+                      end
+  #entry{fullName = Entry#entry.fullName, phones = Phones, emails = Emails}
+.
 
 createAddressBook() ->
   [].
 
-mayAdd(FilterFunction, AddressBook) ->
-  case lists:filter(FilterFunction, AddressBook) of
-    [] -> true;
-    _  -> false
-  end.
-
 addContact(AddressBook, FirstName, LastName) ->
-  NameFilter = fun(CurrentEntry) -> CurrentEntry#entry.fullName == {FirstName, LastName} end,
-  case mayAdd(NameFilter, AddressBook) of
-    true  -> [#entry{fullName = {FirstName, LastName}} | AddressBook];
+  case getBy(fullName, AddressBook, {FirstName, LastName}) of
+    []  -> [#entry{fullName = {FirstName, LastName}} | AddressBook];
     _ -> AddressBook
   end.
 
-addEmail(AddressBook, FirstName, LastName, Email) ->
-  EmailFilter = fun(CurrentEntry) -> lists:member(Email, CurrentEntry#entry.emails) end,
-  case mayAdd()
+addValue(FieldType, AddressBook, ValueToAdd, FirstName, LastName) ->
+  case getBy(fullName, AddressBook, {FirstName, LastName}) of
+    [] -> case getBy(FieldType, AddressBook, ValueToAdd) of
+            [] -> [modifyEntry(FieldType, add, #entry{fullName = {FirstName, LastName}}, ValueToAdd) | AddressBook];
+            _ -> AddressBook
+          end ;
+    [EntryWithSameName] -> case getBy(FieldType, AddressBook, ValueToAdd) of
+                             [] ->
+                               ModifiedEntry = modifyEntry(FieldType, add, EntryWithSameName, ValueToAdd),
+                               [ModifiedEntry | AddressBook -- [EntryWithSameName]] ;
+                             _ -> AddressBook
+                           end
+  end
+.
 
+addEmail(AddressBook, FirstName, LastName, Email) ->
+  case getBy(fullName, AddressBook, {FirstName, LastName}) of
+    [] -> case getBy(emil, AddressBook, Email) of
+            [] -> [#entry{fullName = {FirstName, LastName}, emails = [Email]} | AddressBook];
+            _ -> AddressBook
+          end ;
+    [EntryWithSameName] -> case getBy(emil, AddressBook, Email) of
+                            [] ->
+                              ModifiedEntry = #entry{
+                                fullName = {FirstName, LastName},
+                                emails = [Email | EntryWithSameName#entry.emails ],
+                                phones = EntryWithSameName#entry.phones
+                              },
+                              [ModifiedEntry | AddressBook -- [EntryWithSameName]] ;
+                            _ -> AddressBook
+                           end
+  end.
+
+addPhone(AddressBook, FirstName, LastName, Phone) ->
+  case getBy(fullName, AddressBook, {FirstName, LastName}) of
+    [] -> case getBy(phone, AddressBook, Phone) of
+            [] -> [#entry{fullName = {FirstName, LastName}, phones = [Phone]} | AddressBook];
+            _ -> AddressBook
+          end ;
+    [EntryWithSameName] -> case getBy(phone, AddressBook, Phone) of
+                             [] ->
+                               ModifiedEntry = #entry{
+                                 fullName = {FirstName, LastName},
+                                 phones = [Phone | EntryWithSameName#entry.phones ],
+                                 emails = EntryWithSameName#entry.emails
+                               },
+                               [ModifiedEntry | AddressBook -- [EntryWithSameName]] ;
+                             _ -> AddressBook
+                           end
+  end.
+
+removeContact(AddressBook, FirstName, LastName) ->
+  case getBy(fullName, AddressBook, {FirstName, LastName}) of
+    [EntryFound] -> AddressBook - [EntryFound] ;
+    _ -> AddressBook
+  end
+.
+
+removeEmail(AddressBook, Email) ->
+  case getBy(emil, AddressBook, Email) of
+    [EntryFound] ->
+      ModifiedEntry = #entry{
+        fullName = EntryFound#entry.fullName,
+        phones = EntryFound#entry.phones,
+        emails = EntryFound#entry.emails -- [Email]
+      },
+      [ModifiedEntry | AddressBook -- [EntryFound]]
+  end.
+
+getEmails(AddressBook, FirstName, LastName) ->
+  case getBy(fullName, AddressBook, {FirstName, LastName}) of
+    [SomeEntry] -> SomeEntry#entry.emails ;
+    _ -> []
+  end
+.
+
+getPhones(AddressBook, FirstName, LastName) ->
+  case getBy(fullName, AddressBook, {FirstName, LastName}) of
+    [SomeEntry] -> SomeEntry#entry.phones;
+    _ -> []
+  end
+.
+
+findByEmail(AddressBook, Email) ->
+  case getBy(emil, AddressBook, Email) of
+    [FoundEntry] -> FoundEntry#entry.fullName ;
+    _ -> recordNotFound
+  end
+.
+
+findByPhone(AddressBook, Phone) ->
+  case getBy(phone, AddressBook, Phone) of
+    [FoundEntry] -> FoundEntry#entry.fullName ;
+    _ -> recordNotFound
+  end
+.
--- src/addressBookTest.erl
+++ src/addressBookTest.erl
@@ -8,27 +8,83 @@
 %%%-------------------------------------------------------------------
 -module(addressBookTest).
 -include_lib("eunit/include/eunit.hrl").
--import(addressBook, [createAddressBook/0, addContact/3]).
+-import(addressBook, [createAddressBook/0, addContact/3, addEmail/4]).
 -author("yorg").
 
 %% API
 -compile(export_all).
+
+getNamesOnlyAddressBook() ->
+  Fn1 = "Name",
+  Sn1 = "Surname",
+  Fn2 = "OtherName",
+  Sn2 = "OtherSurname",
+  Fn3 = "YetOtherName",
+  Sn3 = "YetOtherSurname",
+  FirstElement = {entry, {Fn1, Sn1}, [], []},
+  SecontElement = {entry, {Fn2, Sn2}, [], []},
+  ThirdElement = {entry, {Fn3, Sn3}, [], []},
+
+  [FirstElement | [SecontElement | [ThirdElement]]].
 
 createAddressBook_test() -> [] = createAddressBook().
 
 addContact_test() ->
 
   %% given
-  FirstElement = {entry, {"Name", "Surname"}, [], []},
-  SecontElement = {entry, {"OtherName", "OtherSurname"}, [], []},
-
+  [FirstElement, SecondElement, ThirdElement] = getNamesOnlyAddressBook(),
+  { _, {Fn1, Sn1}, _, _ } = FirstElement,
+  { _, {Fn2, Sn2}, _, _ } = SecondElement,
+  { _, {Fn3, Sn3}, _, _ } = ThirdElement,
 
   %% when
-  OneElementList = addContact([], "Name", "Surname"),
-  TwoElementList = addContact(OneElementList, "OtherName", "OtherSurname"),
+  OneElementList = addContact([], Fn1, Sn1),
+  TwoElementList = addContact(OneElementList, Fn2, Sn2),
+  ThreeElementList = addContact(TwoElementList, Fn3, Sn3),
+  ListSupposedToNotContainDuplicatedElements = addContact(
+    addContact(
+      addContact(ThreeElementList, Fn3, Sn3), Fn1, Sn1
+    ), Fn2, Sn2
+  ),
 
   %% then
   ?assert(OneElementList == [FirstElement]),
-  ?assert(OneElementList == addContact(OneElementList, "Name", "Surname")),
+  ?assert(OneElementList == addContact(OneElementList, Fn1, Sn1)),
+
   ?assert(lists:member(FirstElement, TwoElementList)),
-  ?assert(lists:member(SecontElement, TwoElementList)).
+  ?assert(lists:member(SecondElement, TwoElementList)),
+
+  ?assert(lists:member(FirstElement, ThreeElementList)),
+  ?assert(lists:member(SecondElement, ThreeElementList)),
+  ?assert(lists:member(ThirdElement, ThreeElementList)),
+  ?assertEqual(length(ListSupposedToNotContainDuplicatedElements), 3),
+  ?assertEqual(ThreeElementList, ListSupposedToNotContainDuplicatedElements)
+.
+
+%%
+%% addEmail_test() ->
+%%
+%%   %% given
+%%   [FirstElement, SecondElement, ThirdElement] = getNamesOnlyAddressBook(),
+%%   { _, {Fn1, Sn1}, _, _ } = FirstElement,
+%%   { _, {Fn2, Sn2}, _, _ } = SecondElement,
+%%   { _, {Fn3, Sn3}, _, _ } = ThirdElement,
+%%   FirstEmail = "a@b.c",
+%%   SecondEmail = "c@b.a",
+%%   ThirdEmail = "x@y.z",
+%%
+%%   FindByName = fun(AddressBook, Name) -> [X || X <- AddressBook, fun(X) -> {_, XName, _, _} = X, XName == Name end] end,
+%%   FindEmailFun = fun(Entry, Email) ->
+%%     {_, _, _, Emails} = Entry,
+%%     lists:member(Email, Emails)
+%%   end,
+%%   FindByEmail = fun(AddressBook, Name) -> [X || X <- AddressBook, FindEmailFun] end,
+%%
+%%   %% when
+%%   [FirstElementWithEmail] = addEmail([FirstElement], Fn1, Sn1, FirstEmail),
+%%   [FirstElementWithNoDuplicatedEmail] = addEmail([FirstElementWithEmail], Fn1, Sn1, FirstEmail),
+%%   ok
+%%
+%%   %% then
+%%
+%% .
